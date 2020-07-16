437cd5e3a598a7fa4e047b5173df3c45b7a62bc3
GeorgeWL <georgelangham1990@gmail.com>
2020-07-17 00:50:42 +0100

    add config interface

---
+++ server/interfaces/config.interface.ts
@@ -0,0 +1,9 @@
+export default interface IConfigFirebase {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+};
