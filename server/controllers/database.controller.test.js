57e9ab274f0a9583c24e70bf7391fdaa107d4e1d
GeorgeWL <georgelangham1990@gmail.com>
2020-07-17 00:42:35 +0100

    empty device controller tests

---
+++ server/controllers/database.controller.test.js
@@ -0,0 +1,8 @@
+describe('Name of the group', () => {
+  it('should get a device when provided with an id', () => {
+
+  });
+  it('should get device with timestamps if provided with id and options.timestamps', () => {
+
+  })
+});
