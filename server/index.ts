3b3bf165802502a1cdf8c8dea7e5a245cb114484
GeorgeWL <georgelangham1990@gmail.com>
2020-07-17 00:49:31 +0100

    update index config on server

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,28 @@
+import * as dotenv from 'dotenv';
+import helmet from 'helmet'; //useful helper library - makes require less setup for security
+import cors from 'cors'; //security
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
+import devicesRoute from './routes/devices.route';
 
 dotenv.config();
-
 const app = express();
+// app config
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
-const port = parseInt(process.env.PORT_NUMBER as string, 10)
+// on local use default port, else use passed-in from env
+let portNumber: number;
+if (process.env.NODE_ENV === 'local') {
+  portNumber = 4000;
+} else {
+  portNumber = parseInt(process.env.PORT_NUMBER as string, 10)
+}
 
-app.use("/api", (req: Request, res: Response): void => {
-  res.send("You have reached the API");
+app.use('/api/devices', devicesRoute)
+
+app.get("/api", (req: Request, res: Response): void => {
+  res.status(200).send({ isTest: true });
 })
 
-app.listen(port, () => `Server listening on port ${port}!`)
+app.listen(portNumber, () => `Server listening on port ${portNumber}!`)
