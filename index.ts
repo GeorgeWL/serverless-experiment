6848ceb1550666b278f4a081bce366fcb4f07000
GeorgeWL <georgelangham1990@gmail.com>
2020-07-16 18:22:26 +0100

    init api

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const port = parseInt(process.env.PORT_NUMBER as string, 10)
-
-
+app.get("/api", (req: Request, res: Response): void => {
+  res.send("You have reached the API!");
+})
 app.listen(port, () => `Server listening on port ${port}!`)
