12d3019227733fdb7f502db99cbe00ac87fbffc4
GeorgeWL <georgelangham1990@gmail.com>
2020-07-19 14:09:56 +0100

    build failing tewsts and error catching first

--- server/controllers/database.controller.test.js
+++ server/controllers/database.controller.test.js
@@ -1,8 +1,56 @@
-describe('Name of the group', () => {
-  it('should get a device when provided with an id', () => {
+import { getDevices, getDeviceById, createDevice, updateDevice, removeDeviceById } from "./database.controller";
+const getDaysAgoFromNow = (days) => {
+  const date = new Date();
+  date.setDate(date.getDate() - days)
+  return new Date(date)
+}
+describe('Succesful Requests', () => {
+  it('should get all devices, without options object', async () => {
+    expect(await getDevices()).toEqual();
+  });
+  it('should get all devices, with options which filter the devices by date', async () => {
+    expect(await getDevices({ minDate: getDaysAgoFromNow(4) })).toEqual();
+    expect(await getDevices({ maxDate: getDaysAgoFromNow(1) })).toEqual();
+  })
+  it('should create a device when provided with a valid id and data', () => {
+
+  })
+  it('should update a device when provided with a valid existing id and data', () => {
+
+  })
+  it('should get a device when provided with a valid id', () => {
 
   });
-  it('should get device with timestamps if provided with id and options.timestamps', () => {
+  it('should remove device when provided with a valid id', () => {
 
+  })
+});
+describe('Failure Tests', () => {
+  it('should error and not return for getDevices if provided with a malformed options', async () => {
+    expect(await getDevices({}).catch(err => err.message)).toEqual('If provide options, minDate and/or maxDate is required');
+  })
+  it('should fail to get a device when provided with invalid or none-existent id', async () => {
+    expect(await getDeviceById(undefined).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await getDeviceById(null).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await getDeviceById({}).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await getDeviceById(1).catch(err => err.message)).toEqual('Provide a deviceID');
+  });
+  it('should fail to create a device when provided with invalid or none-existent id', async () => {
+    expect(await createDevice(undefined).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await createDevice(null).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await createDevice({}).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await createDevice(1).catch(err => err.message)).toEqual('Provide a deviceID');
+  });
+  it('should fail to update a device when provided with invalid or none-existent id', async () => {
+    expect(await updateDevice(undefined).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await updateDevice(null).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await updateDevice({}).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await updateDevice(1).catch(err => err.message)).toEqual('Provide a deviceID');
+  });
+  it('should fail to remove device when provided with invalid or none-existent id', async () => {
+    expect(await removeDeviceById(undefined).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await removeDeviceById(null).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await removeDeviceById({}).catch(err => err.message)).toEqual('Provide a deviceID');
+    expect(await removeDeviceById(1).catch(err => err.message)).toEqual('Provide a deviceID');
   })
 });
--- server/controllers/database.controller.ts
+++ server/controllers/database.controller.ts
@@ -1,17 +1,39 @@
 import DatabaseService from "../services/database.service";
 import IConfigFirebase from "../interfaces/config.interface";
+import * as dotenv from 'dotenv';
 
-const serviceConfig: IConfigFirebase = JSON.parse(process.env.FIREBASE_CONFIG as string);
-
+dotenv.config();
+const unparsedConfig = process.env.FIREBASE_CONFIG as string;
+const serviceConfig: IConfigFirebase = JSON.parse(unparsedConfig);
 const db = new DatabaseService(serviceConfig);
 
-export const getDeviceById = async (deviceId: string) => {
-  if (!deviceId) { throw new Error('Provide a deviceID'); }
-  throw new Error('Device Not Found');
+export interface IOptions {
+  minDate: Date;
+  maxDate: Date;
+  withRelations: boolean;
 }
 
+export const getDeviceById = async (deviceId: string) => {
+  if (typeof deviceId !== 'string') {
+    throw new Error('Provide a deviceID');
+  }
+  try {
+    const device = await db.DeviceStore.doc(deviceId).get()
+      .catch(err => {
+        console.error('error', err);
+        throw new Error('Device Not Found');
+      })
+    return device;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// technically in the JSON DB used, Create-Update are the same action, as it's evetnually consistent
 export const createDevice = async (deviceId: string, data: object) => {
-  if (!deviceId) { throw new Error('Provide a deviceID'); }
+  if (!deviceId || typeof deviceId !== 'string') {
+    throw new Error('Provide a deviceID');
+  }
   if (!data) {
     throw new Error('No Data Provided');
   }
@@ -19,7 +41,9 @@ export const createDevice = async (deviceId: string, data: object) => {
 }
 
 export const updateDevice = async (deviceId: string, data: object) => {
-  if (!deviceId) { throw new Error('Provide a deviceID'); }
+  if (!deviceId || typeof deviceId !== 'string') {
+    throw new Error('Provide a deviceID');
+  }
   if (!data) {
     throw new Error('No Data Provided');
   }
@@ -27,6 +51,15 @@ export const updateDevice = async (deviceId: string, data: object) => {
 }
 
 export const removeDeviceById = async (deviceId: string) => {
-  if (!deviceId) { throw new Error('Provide a deviceID'); }
+  if (!deviceId || typeof deviceId !== 'string') {
+    throw new Error('Provide a deviceID');
+  }
   throw new Error('Device Not Found, Unable to delete');
+}
+
+export const getDevices = async (options?: IOptions) => {
+  if (options && !options.minDate || options && !options.maxDate) {
+    throw new Error('If provide options, minDate and/or maxDate is required');
+  }
+  throw new Error('Unable to fetch devices');
 }
